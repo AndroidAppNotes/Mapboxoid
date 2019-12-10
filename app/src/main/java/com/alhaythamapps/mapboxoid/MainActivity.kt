a34fac22f9bbe2ad43da4414afd5e1cd69feb0f1
Alhaytham Alfeel <alhaytham.alfeel@andela.com>
2019-12-10 19:49:49 +0200

    Mapbox installed.

--- app/src/main/java/com/alhaythamapps/mapboxoid/MainActivity.kt
+++ app/src/main/java/com/alhaythamapps/mapboxoid/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.alhaythamapps.mapboxoid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+
 
 class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.access_token))
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { map ->
+            map.setStyle(Style.MAPBOX_STREETS) { style ->
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        mapView?.onSaveInstanceState(outState)
     }
 }
